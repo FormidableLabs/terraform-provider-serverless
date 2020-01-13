97949ae48b5a8eaa9e59e07be9a5cfe6e36d2e52
Dillon Mulroy <dillon.mulroy@formidable.com>
2020-01-13 14:35:09 -0500

    add getter interface, serverless struct, serverless constructor, and run method, additional comments/questions

--- serverless/resource_deployment.go
+++ serverless/resource_deployment.go
@@ -32,8 +32,85 @@ import (
 
 // Questions
 // - Follow up on `hashServerlessDir` in the case when multiple things need hashed
+// - How does %w work in fmt.Errorf
 //
-//
+
+type getter interface {
+	Get(key string) interface{}
+}
+
+// TODO investigate if we can remove some of these fields if they're only being used
+// to construct other values in one place (move that logic to the constructor)
+type serverless struct {
+	binDir     string
+	binPath    string
+	configDir  string
+	packageDir string
+	stage      string
+	hash       string
+	args       []string
+}
+
+func (s serverless) run(command string) error {
+	args := []string{
+		command, "-s",
+		s.stage,
+	}
+
+	if command == "deploy" || command == "package" {
+		args = append(args, "-p", s.packageDir)
+	}
+
+	args = append(args, s.args...)
+
+	cmd := exec.Command(s.binPath, args...)
+	cmd.Dir = s.configDir
+
+	output, err := cmd.CombinedOutput()
+
+	if err != nil {
+		return fmt.Errorf("%v\n%w", string(output), err)
+	}
+
+	return nil
+}
+
+func newServerless(resource getter) serverless {
+	resourceArgs := resource.Get("args").([]interface{})
+	args := make([]string, len(resourceArgs))
+
+	for _, arg := range resourceArgs {
+		args = append(args, arg.(string))
+	}
+
+	var binPath string
+	var suffix string
+
+	binDir := resource.Get("serverless_bin_dir").(string)
+	configDir := resource.Get("config_dir").(string)
+
+	if binDir == "" {
+		binPath = filepath.Join(configDir, "node_modules", ".bin")
+	} else {
+		binPath = binDir
+	}
+
+	if runtime.GOOS == "windows" {
+		suffix = ".cmd"
+	}
+
+	binPath = filepath.Join(binPath, fmt.Sprintf("serverless%s", suffix))
+
+	return serverless{
+		binDir:     binDir,
+		binPath:    binPath,
+		configDir:  configDir,
+		packageDir: resource.Get("package_dir").(string),
+		stage:      resource.Get("stage").(string),
+		hash:       resource.Get("package_hash").(string),
+		args:       args,
+	}
+}
 
 type serverlessConfig struct {
 	Service string
